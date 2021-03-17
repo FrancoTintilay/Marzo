320cf7e0179ac5ab110230b8d4b924ec740ff18c
admin <admin@LAPTOP-0PALGUM3>
2021-03-16 22:49:06 -0300

    CAPAS,VISTAS Y CONEXION DE BD

---
+++ src/main/java/ar/edu/unju/edm/modelo/Usuario.java
@@ -0,0 +1,155 @@
+package ar.edu.unju.edm.modelo;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@Entity
+public class Usuario implements Serializable {
+	/**
+	 * 
+	 */	
+	private static final long serialVersionUID= 1L;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private Long id;
+	@Column
+	String nombreusuario;
+	@Column
+	String password;
+	@Column
+	int dni;
+	@Column
+	String nombre;
+	@Column
+	String apellido;
+	@Column
+	String tipo;
+	public Usuario() {
+		
+	}
+	public Usuario(Long id, String nombreusuario, String password, int dni, String nombre, String apellido,
+			String tipo) {
+		super();
+		this.id = id;
+		this.nombreusuario = nombreusuario;
+		this.password = password;
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.tipo = tipo;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombreusuario() {
+		return nombreusuario;
+	}
+	public void setNombreusuario(String nombreusuario) {
+		this.nombreusuario = nombreusuario;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getDni() {
+		return dni;
+	}
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nombreusuario=" + nombreusuario + ", password=" + password + ", dni=" + dni
+				+ ", nombre=" + nombre + ", apellido=" + apellido + ", tipo=" + tipo + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((apellido == null) ? 0 : apellido.hashCode());
+		result = prime * result + dni;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((nombreusuario == null) ? 0 : nombreusuario.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((tipo == null) ? 0 : tipo.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (apellido == null) {
+			if (other.apellido != null)
+				return false;
+		} else if (!apellido.equals(other.apellido))
+			return false;
+		if (dni != other.dni)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (nombreusuario == null) {
+			if (other.nombreusuario != null)
+				return false;
+		} else if (!nombreusuario.equals(other.nombreusuario))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (tipo == null) {
+			if (other.tipo != null)
+				return false;
+		} else if (!tipo.equals(other.tipo))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
